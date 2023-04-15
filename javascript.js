5ea4927a15239ff47986fb5f698b91ab2b86d106
Jim Ng <jimng@Jims-MacBook-Pro.local>
2023-04-15 16:21:03 +1000

    finished branch

--- javascript.js
+++ javascript.js
@@ -2,56 +2,96 @@ const winner_map = {
   "rock":"scissors",
   "scissors":"paper",
   "paper":"rock"
-}
+};
+
+let games = 0
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice() {
     const choices = Object.keys(winner_map)
     return choices[Math.floor(Math.random() * choices.length)]
-  }
+  };
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase() 
-    computerSelection = computerSelection.toLowerCase() 
+function playRound(playerInput, computerInput) {
 
-    if (playerSelection == computerSelection) {
-      return 0
-    } else if (winner_map[playerSelection] == computerSelection) {
-      return 1
+    if (playerInput == computerInput) {
+      return "draw"
+    } else if (winner_map[playerInput] == computerInput) {
+      return "win"
     } else {
-      return -1
+      return "loss"
     }
-}
+};
 
-function game() {
-  let playerScore = 0
-  let ComputerScore = 0
-  let games = 0
+function updateScores(result) {
+  if (result == "win") {
+    playerScore += 1;
+    document.getElementById("player-score").innerHTML = playerScore
+  } else if (result == "loss") {
+    computerScore += 1
+    document.getElementById("computer-score").innerHTML = computerScore
+  }
+};
 
-  while (games != 5) {
-    let playerInput = prompt("What is your move:");
+
+function updateRoundResult(result) {
+  let result_display = document.getElementById("round-result")
+
+  if (result == "win") {
+    result_display.innerHTML = "You Win!"
+  } else if (result == "loss") {
+    result_display.innerHTML = "You Lose!"    
+  } else {
+    result_display.innerHTML = "Draw!"   
+  }
+};
+
+function updateBoard(playerInput, computerInput) {
+  document.getElementById("player-move").innerHTML = playerInput
+  document.getElementById("computer-move").innerHTML = computerInput
+};
+
+function gameEnd(totalGames) {
+  if (games == totalGames) {
+    return 1
+  }
+  
+  games += 1
+
+  return 0
+};
+
+
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    let playerInput = button.id
     let computerInput = getComputerChoice()
 
-    let result = playRound(playerInput, computerInput)
+    console.log(playerInput)
+    console.log(computerInput)
 
-    if (result == 0) {
-      continue
-    } else if (result == 1) {
-      playerScore += 1
-    } else {
-      computerScore += 1
+    let result_round = playRound(playerInput, computerInput)
+
+    updateScores(result_round)
+    updateRoundResult(result_round)
+    updateBoard(playerInput, computerInput)
+
+    let gameEndStatus = gameEnd(5)
+
+    if (gameEndStatus == 1) {
+      if (playerScore > computerScore) {
+        alert("you are the winner!")
+      } else {
+        alert("you lose!")
+      }
     }
 
-    games += 1
-  }
-
-  if (playerScore > ComputerScore) {
-    console.log("you are the winner!")
-  } else {
-    console.log("you lose!")
-  }
-}
-
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', function (e) {
-  e.target.style.background = 'blue';
+  });  
 });
